ebb7aafb9052180aa85ececa3225b9d1949f3383
Tagir.A <tccup1@gmail.com>
2017-03-01 16:26:29 +0300

    add partial modular css and gitignore for node_module

--- js/board.js
+++ js/board.js
@@ -1,12 +1,13 @@
 import React from "react"
+import $ from "../css/board.css"
 
 export default function Board(props) {
     return (
-        <div class="board">
+        <div class={$.board}>
             {props.board.map((item, i) => {
                 let cls;
-                if (i === props.winline[0] || i === props.winline[1] || i === props.winline[2]) { cls="cell winline"}
-                else { cls="cell"}
+                if (i === props.winline[0] || i === props.winline[1] || i === props.winline[2]) { cls=$.cell + " " + $.winline}
+                else { cls=$.cell}
                 return <button
                     id={i}
                     key={i}
